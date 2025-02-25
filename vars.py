313163245872e5c3c31e1ea83a0a1d7e316f2ed2
MR-BRIJESH <brijeshyadavhtw7050@gmail.com>
2025-02-25 22:41:46 +0530

    Update vars.pybk
    Signed-off-by: MR-BRIJESH <brijeshyadavhtw7050@gmail.com>

--- vars.py
+++ vars.py
@@ -6,4 +6,4 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "7650861097"))
 API_HASH = environ.get("API_HASH", "be3a4b6da22a66db83995afc70a733a1")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7650861097:AAFc1N4BNaTvsa5EuviUeRnFBHLziHDSrVQ")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7727306885:AAFON-FxJmTjYFXzvNvQPFXMj5kECu714ho")
